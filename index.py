7565c5de459e79ae211c93709192248606075d2e
Aman Singh Rana <107622127+aman7cr74@users.noreply.github.com>
2026-07-14 23:29:55 +0530

    Add 'v77' to the list of items

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
    
 df = pd.DataFrame() 
 print(df)
-lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'v9'] 
+lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'v9','v77'] 
   
 df = pd.DataFrame(lst) 
 print(df)
