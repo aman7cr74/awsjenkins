cd27e92c540f19cf850b789120ccb947a2e548a1
Aman Singh Rana <107622127+aman7cr74@users.noreply.github.com>
2026-07-14 23:25:25 +0530

    Uncomment DataFrame creation and printing

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
    
 df = pd.DataFrame() 
 print(df)
-# lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks'] 
+lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks'] 
   
-# df = pd.DataFrame(lst) 
-# print(df)
+df = pd.DataFrame(lst) 
+print(df)
